a605f989e4703d0d532bdf3c913186e72566301b
Diego Romero <arias.dromero@gmail.com>
2023-10-13 11:26:29 -0600

    Changes on App component and index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>test</p>
+    <div className='app'>
+      <header className='header'>
+        <div className='logo'>
+          <h1>Logo</h1>
+        </div>
+        <form>
+          <input type='text' placeholder='Search' />
+          <button type='submit'>Search</button>
+        </form>
       </header>
+      
+      <main className='main'>
+        {/* Display publications from Reddit API */}
+        { /* You can map through the posts and render PostListItem components */ }
+      </main>
+
+      <aside className='sidebar'>
+        <h2>Subreddits</h2>
+        <ul>
+          { /* Display important subreddits from Reddit API*/}
+          { /* You can map through the subreddits and render list items */}
+        </ul>
+      </aside>
     </div>
-  );
+  );  
 }
+  
+
 
 export default App;
